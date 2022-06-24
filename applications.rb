b66a4a7f548dae7ce3fc4dc7c63774639ff13bde
Keith Laster <lasterk9@gmail.com>
2022-06-24 13:43:03 -0500

    Utilized the 'toggle around' method to execute code in one state and then toggle back to the standard state.

---
+++ applications.rb
@@ -0,0 +1,53 @@
+class Application
+  attr_accessor :environment
+
+  def initialize
+    @environment = :development
+  end
+
+  def connect_to_database
+    puts "Connecting to #{@environment} database..."
+  end
+
+  def handle_request
+    puts "Handling #{@environment} request..."
+  end
+
+  def write_to_log
+    puts "Writing to #{@environment} log file..."
+  end
+
+  def in_environment(new_environment)
+    old_enviroment = @environment
+    @environment = new_environment
+    yield
+  rescue Exception => e
+    puts e.message
+  ensure @environment = old_enviroment
+         puts "Reset environment to #{@environment}"
+  end
+end
+
+app = Application.new
+app.connect_to_database
+app.handle_request
+app.write_to_log
+
+# app.in_environment do
+#   app.connect_to_database
+#   app.handle_request
+#   raise 'Boom!'
+#   app.write_to_log
+# end
+
+app.in_environment(:production) do
+  app.connect_to_database
+  app.handle_request
+  app.write_to_log
+end
+
+app.in_environment(:test) do
+  app.connect_to_database
+  app.handle_request
+  app.write_to_log
+end
